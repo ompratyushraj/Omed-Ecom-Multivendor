9bd83290f01896fe09bb1e2df27001e371e61f6a
ompratyushraj <ompratyushraj@gmail.com>
2025-09-19 14:40:47 +0530

    addproductui

--- Omed-Ecommerce-Multivendor-Frontend/src/seller/sellerpages/sellerpayment/SellerPayment.tsx
+++ Omed-Ecommerce-Multivendor-Frontend/src/seller/sellerpages/sellerpayment/SellerPayment.tsx
@@ -1,9 +1,24 @@
+import { Button, Card, Divider } from '@mui/material'
 import React from 'react'
+import TransactionTable from './TransactionTable'
 
 const SellerPayment = () => {
   return (
     <div>
-      <h1>Seller Payment</h1>
+      <div className='space-y-5'>
+        <Card className='rounded-md space-y-4 p-5'>
+          <h1 className='text-gray-600 font-medium'>Total Earning</h1>
+          <h1 className='font-bold text-xl pb-1'>₹11644</h1>
+          <Divider />
+          <p className='text-gray-600 font-medium pt-1'>Last Payment : <strong>₹0</strong></p>
+        </Card>
+        <div className='mt-20 space-y-3'>
+          <Button variant='contained'>
+            Transaction
+          </Button>
+          <TransactionTable />
+        </div>
+      </div>
     </div>
   )
 }
